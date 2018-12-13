8081777e201a3f5ba90f87ea87ea442558798388
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2018-12-13 10:49:02 +0200

    feat: implement entry point for a project
    Closes: https://github.com/o-rumiantsev/Dirsync/issues/15

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strcit';
+
+const Client = require('./lib/client');
+const Server = require('./lib/server');
+const Watcher = require('./lib/watcher');
+
+// Connect to server which is sharing its file system
+//   url - <string>, server url address
+//   cb - <Function>, callback
+// Returns: <Client>
+const connect = (url, cb) => {
+  const client = new Client();
+  client.connect(utl);
+  if (cb) client.on('connect', cb);
+  return client;
+};
+
+// Create server to share local file system
+//   options - <Object>, server options
+//     port - <number>, listening port,
+//         optional, default: 8080
+//     host - <string>, listening host,
+//         optional, default: 'localhost'
+//     dir - <string>, path to directory to be synced,
+//         optional, default: current working directory
+//     ignore - <RegExp>, regexp for file names,
+//         which should be ignored, optional
+// Returns: <Server>
+const share = options => new Server(options);
+
+module.exports = {
+  connect,
+  share,
+  Watcher,
+};
+
